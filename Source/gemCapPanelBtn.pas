ee0eeb2bd9aa36236542905738705a6b77fc4f83
Gary E <garymcin@gmail.com>
2021-02-04 10:11:42 -0700

    Update gemCapPanelBtn.pas

--- Source/gemCapPanelBtn.pas
+++ Source/gemCapPanelBtn.pas
@@ -1,5 +1,44 @@
 unit gemCapPanelBtn;
 
+(*
+The Caption panel idea is from Jedi tjvCaptionPanel. I modified the panel
+to use VCL componenets.  I made this a compound componenet by adding a
+TImage to it. The idea behind adding the TImage is it allows for Image_Align,
+Image_Center, Image_AutoSize, Image_Proportional, and Image_Stretch. So an image
+of different sizes can be used without resizing image.  The component
+has a different look to it than a speed button, that I like.
+
+
+BSD 3-Clause License
+
+Copyright (c) 2021, Gary E McIntosh
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*)
 interface
 uses
   Winapi.Windows, Winapi.Messages,
