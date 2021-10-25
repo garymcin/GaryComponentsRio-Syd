f2e274f7208624bb07abbe2223001e0fe69289b5
garymcin <garymcin@gmail.com>
2021-10-25 16:24:05 -0600

    fixing new bug
    There can be a chance that for a day or date that clients on the same route plan address can be on different routes.  This will cause an exception in the ploy and marker info tables.

--- ComponentRegistration/GarysComponentsReg.pas
+++ ComponentRegistration/GarysComponentsReg.pas
@@ -12,7 +12,8 @@ uses
  GEMProcessTimer, gemArrow, SelColVisGrid, ColorPanelBtn, Preview, ColorButton,
  gemCustomButton, adpMRU, GEMDBLabel, URLLabel, FindFile, adpDBEdit, GemDBGrid,
  gemDBComboBox, gemDBLookupComboBox, PJVersionInfo, GEMColorButton, GEMTrafficLight,
- GEMTrafficLight2, uAutoComplete,  gemCapPanelBtn, GEMCustomColorsDialog, CnColorGrid;
+ GEMTrafficLight2, uAutoComplete,  gemCapPanelBtn, GEMCustomColorsDialog, CnColorGrid,
+ SimpleGraph;
 
 procedure Register;
 begin
@@ -23,15 +24,15 @@ begin
   var VerType := 'Garys Stuff Rio';
 {$IFEND}
   RegisterComponents(VerType, [TGEMAppUpdater, TGemMruList, TStatusBarPro,
-                                     TgemResistor, TGEMNxQuery, TGEMNxTable, TColorButton,
-                                     TGEMProcessTimer, TgemArrow, TSelColVisGridBtn,
-                                     TColorPanelSpecial, TPrintPreview, TThumbnailPreview,
-                                     TPaperPreview, TgemShapeBtn, TadpMRU, TGEMDBLabel,
-                                     TGEMUrlLabel, TFindFile, TGemDBGrid, TadpDBEdit,
-                                     TgemDBComboBox, TgemDBLookupComboBox, TPJVersionInfo,
-                                     TGEMColorButton, TGEMTrafficLight, TGEMTrafficLight2,
-                                     TgemCapPanelBtn, TAutoCompleteEdit, TCnColorGrid,
-                                     TGEMShape, TGEMShape2, TGEMColorDialog]);
+                               TgemResistor, TGEMNxQuery, TGEMNxTable, TColorButton,
+                               TGEMProcessTimer, TgemArrow, TSelColVisGridBtn,
+                               TColorPanelSpecial, TPrintPreview, TThumbnailPreview,
+                               TPaperPreview, TgemShapeBtn, TadpMRU, TGEMDBLabel,
+                               TGEMUrlLabel, TFindFile, TGemDBGrid, TadpDBEdit,
+                               TgemDBComboBox, TgemDBLookupComboBox, TPJVersionInfo,
+                               TGEMColorButton, TGEMTrafficLight, TGEMTrafficLight2,
+                               TgemCapPanelBtn, TAutoCompleteEdit, TCnColorGrid,
+                               TGEMShape, TGEMShape2, TGEMColorDialog, TSimpleGraph]);
 end;
 
 end.
